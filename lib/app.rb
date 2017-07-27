883b826d1181939747a6594eb1cf7e1df315da4b
Bailey Kursar <bkursar@gmail.com>
2017-07-27 13:38:53 +0100

    Made the signup form with email and password and made the first test pass

--- lib/app.rb
+++ lib/app.rb
@@ -2,16 +2,16 @@ require 'sinatra/base'
 require_relative 'models/speaker'
 
 class Speakup < Sinatra::Base
-  get '/' do
-    @speakers = Speaker.all
-    erb :'speakers'
-  end
+  # get '/' do
+  #   @speakers = Speaker.all
+  #   erb :'speakers'
+  # end
 
   get '/signup' do
     erb :'signup'
   end
 
-  get '/profile' do
-    erb :'profile'
-  end
+  # get '/profile' do
+  #   erb :'profile'
+  # end
 end
